2b92dba0ee8a9ca4c875a50edca2978f3d03407e
alexlangley <msg2alex@gmail.com>
2014-05-05 13:46:27 -0500

    Initial Commit

---
+++ Man's Best Friend/MBFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MBFAppDelegate.h
+//  Man's Best Friend
+//
+//  Created by Alex Langley on 5/5/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Man's Best Friend/MBFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBFViewController.h
+//  Man's Best Friend
+//
+//  Created by Alex Langley on 5/5/14.
+//  Copyright (c) 2014 Truu Bruu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFViewController : UIViewController
+
+@end
